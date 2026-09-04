79e0a1abbc62d3c29a72d224701775f0e0ff998e
c20245793salanap-ui <C20245793.salanap@csav.edu.ph>
2026-09-04 14:38:58 +0800

    Variables (Lets and Const)

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-console.log("Hello World");
+console.log("Hello world.");
 console.log("From script.js file.");
+
+const myName = "ARJAY";
+let age = 20;
+const number = "090946300889";
+const address = "DITO LANG SA TABI TABI";
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${number}`);
+console.log(`Address: ${address}`);
+console.log(67);
